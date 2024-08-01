de9cd47cff3ffc3b8889a26aa1c3ee7db1b7eea7
solvictor <victor.egret.pro@gmail.com>
2024-08-01 20:26:12 +0200

    Parsing WIP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::convert::TryFrom;
 use std::env;
 use std::fmt::{Debug, Display, Error, Formatter};
 
-#[derive(Clone, Debug)]
+#[repr(u8)]
+#[derive(Clone, Debug, Copy)]
 pub enum Color {
     WHITE,
     YELLOW,
@@ -11,74 +13,151 @@ pub enum Color {
     ORANGE,
 }
 
-impl Color {
-    // TODO: #[repr(u8)] on pub enum Color instead?
-    fn from_int(n: u8) -> Option<Color> {
-        match n {
-            0 => Some(Color::WHITE),
-            1 => Some(Color::YELLOW),
-            2 => Some(Color::RED),
-            3 => Some(Color::GREEN),
-            4 => Some(Color::BLUE),
-            5 => Some(Color::ORANGE),
-            _ => None,
+impl TryFrom<u8> for Color {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Color::WHITE),
+            1 => Ok(Color::YELLOW),
+            2 => Ok(Color::RED),
+            3 => Ok(Color::GREEN),
+            4 => Ok(Color::BLUE),
+            5 => Ok(Color::ORANGE),
+            _ => Err(()),
         }
     }
 }
 
-#[derive(Clone, Debug)]
+#[repr(u8)]
+#[derive(Clone, Debug, Copy)]
 pub enum Move {
-    FRONT,
-    RIGHT,
-    UP,
-    BACK,
-    LEFT,
-    DOWN,
+    F,
+    R,
+    U,
+    B,
+    L,
+    D,
+    F2,
+    R2,
+    U2,
+    B2,
+    L2,
+    D2,
+    F3,
+    R3,
+    U3,
+    B3,
+    L3,
+    D3,
 }
 
 impl Move {
-    fn from_int(n: u8) -> Option<Move> {
-        match n {
-            0 => Some(Move::FRONT),
-            1 => Some(Move::RIGHT),
-            2 => Some(Move::UP),
-            3 => Some(Move::BACK),
-            4 => Some(Move::LEFT),
-            5 => Some(Move::DOWN),
+    pub fn from_int(index: u8) -> Option<Move> {
+        match index {
+            0 => Some(Move::F),
+            1 => Some(Move::R),
+            2 => Some(Move::U),
+            3 => Some(Move::B),
+            4 => Some(Move::L),
+            5 => Some(Move::D),
+            6 => Some(Move::F2),
+            7 => Some(Move::R2),
+            8 => Some(Move::U2),
+            9 => Some(Move::B2),
+            10 => Some(Move::L2),
+            11 => Some(Move::D2),
+            12 => Some(Move::F3),
+            13 => Some(Move::R3),
+            14 => Some(Move::U3),
+            15 => Some(Move::B3),
+            16 => Some(Move::L3),
+            17 => Some(Move::D3),
             _ => None,
         }
     }
 
-    fn from_str(s: &str) -> Option<Move> {
-        todo!();
+    pub fn as_int(&self) -> u8 {
+        *self as u8
+    }
+
+    #[allow(dead_code)]
+    fn opposite(&self) -> Move {
+        let mut i = self.as_int();
+        if i < 6 {
+            i += 12;
+        } else if i > 11 {
+            i %= 12;
+        }
+        Move::from_int(i).unwrap()
+    }
+}
+
+impl TryFrom<&str> for Move {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "F" => Ok(Move::F),
+            "F2" => Ok(Move::F2),
+            "F'" | "F’" => Ok(Move::F3),
+            "R" => Ok(Move::R),
+            "R2" => Ok(Move::R2),
+            "R'" | "R’" => Ok(Move::R3),
+            "U" => Ok(Move::U),
+            "U2" => Ok(Move::U2),
+            "U'" | "U’" => Ok(Move::U3),
+            "B" => Ok(Move::B),
+            "B2" => Ok(Move::B2),
+            "B'" | "B’" => Ok(Move::B3),
+            "L" => Ok(Move::L),
+            "L2" => Ok(Move::L2),
+            "L'" | "L’" => Ok(Move::L3),
+            "D" => Ok(Move::D),
+            "D2" => Ok(Move::D2),
+            "D'" | "D’" => Ok(Move::D3),
+            _ => Err(()),
+        }
     }
 }
 
 pub struct Cube {
-    faces: Vec<Vec<Color>>,
+    faces: Vec<Color>,
+    size: usize,
 }
 
 impl Cube {
     pub fn new(n: usize) -> Cube {
         println!("New Cube !");
         let mut faces = Vec::new();
-        for i in 0..6 {
-            faces.push(vec![Color::from_int(i).unwrap(); n * n]);
+        for i in 0..6 * n * n {
+            let face = i / (n * n);
+            // let _y = (i % (n * n)) / n;
+            // let _x = (i % (n * n)) % n;
+            let color = (face as u8).try_into();
+            faces.push(color.unwrap());
         }
-        Cube { faces }
+        Cube { faces, size: n }
     }
 
+    pub fn get_face(&self, face: u8) -> Vec<Color> {
+        let start = face as usize * self.size * self.size;
+        let end = (face as usize + 1) * self.size * self.size;
+        self.faces[start..end].to_vec()
+    }
+
+    // TODO
     pub fn do_move(&self, _move: Move) {
-        todo!();
+        println!("{:?}", _move);
     }
 
     pub fn scramble(&self, sequence: &str) -> Result<(), Error> {
-        let as_moves = sequence.split_whitespace().map(Move::from_str);
+        let as_moves = sequence.split_whitespace().map(Move::try_from);
 
         for mov in as_moves {
             match mov {
-                Some(m) => self.do_move(m),
-                None => return Err(Error),
+                Ok(m) => self.do_move(m),
+                Err(_) => return Err(Error),
             }
         }
 
@@ -102,7 +181,9 @@ fn main() -> Result<(), Error> {
         return Err(Error); // TODO Better error handling
     }
 
-    let cube = Cube::new(2);
+    let cube = Cube::new(3);
+    println!("{}", cube.faces.len());
+
     cube.scramble(&args[1])?;
     println!("{}", cube);
 
