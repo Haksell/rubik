e3a86dd46319cf7447c2082cf559eee55ae5ccec
Haksell <axel.brisse@gmail.com>
2024-11-15 13:37:47 +0100

    manual

--- src/visualizer/mod.rs
+++ src/visualizer/mod.rs
@@ -1,16 +1,14 @@
-mod karaoke;
+// mod karaoke;
 
 use crate::r#move::Move;
-use crate::visualizer::karaoke::{draw_karaoke, karaoke_format};
 use crate::Puzzle;
-use kiss3d::event::{Action, WindowEvent};
+use kiss3d::event::{Action, Key, WindowEvent};
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
 use kiss3d::window::Window;
-use std::time::Instant;
 
 const WINDOW_SIZE: u32 = 1000;
-const MOVE_INTERVAL_MS: u128 = 300;
+// const MOVE_INTERVAL_MS: u128 = 300;
 
 fn refresh_stickers(stickers: &mut Vec<SceneNode>, puzzle: &mut Box<dyn Puzzle>) {
     stickers
@@ -23,7 +21,7 @@ fn refresh_stickers(stickers: &mut Vec<SceneNode>, puzzle: &mut Box<dyn Puzzle>)
 }
 
 // TODO: flag for playground mode
-pub fn visualize(puzzle: &mut Box<dyn Puzzle>, moves: &Vec<Move>, karaoke: bool) {
+pub fn visualize(puzzle: &mut Box<dyn Puzzle>, moves: &Vec<Move>, _karaoke: bool) {
     let mut window = Window::new_with_size("rubik", WINDOW_SIZE, WINDOW_SIZE);
 
     window.set_light(Light::StickToCamera);
@@ -34,44 +32,34 @@ pub fn visualize(puzzle: &mut Box<dyn Puzzle>, moves: &Vec<Move>, karaoke: bool)
     cam.set_dist_step(1.0);
     cam.set_dist(9.6); // TODO: depends on puzzle
 
-    // Disable translation
     cam.rebind_drag_button(None);
-
-    let start = Instant::now();
 
     let mut i: usize = 0;
 
     let mut stickers = puzzle.draw(&mut window);
 
-    let mut text = String::new();
-
-    if karaoke {
-        text = karaoke_format(moves);
-    }
-
     while window.render_with_camera(&mut cam) {
-        if karaoke {
-            draw_karaoke(&text, &start, moves.len(), &mut window);
-        }
-
-        if i < moves.len() {
-            let elapsed = start.elapsed().as_millis();
-            let idx = (elapsed / MOVE_INTERVAL_MS) as usize;
-
-            if idx > i {
-                puzzle.do_move(moves[i]);
-                i = idx;
-
-                refresh_stickers(&mut stickers, puzzle);
-            }
-        } else {
-            for mut event in window.events().iter() {
-                if let WindowEvent::Key(button, Action::Release, mods) = event.value {
-                    if let Ok(move_) = Move::try_from((button, mods)) {
-                        puzzle.do_move(move_);
-                        refresh_stickers(&mut stickers, puzzle);
+        for mut event in window.events().iter() {
+            if let WindowEvent::Key(button, Action::Press, _) = event.value {
+                match button {
+                    Key::Left => {
+                        if i > 0 {
+                            i -= 1;
+                            let inverse_move = moves[i].opposite();
+                            puzzle.do_move(inverse_move);
+                            refresh_stickers(&mut stickers, puzzle);
+                        }
                         event.inhibited = true;
                     }
+                    Key::Right => {
+                        if i < moves.len() {
+                            puzzle.do_move(moves[i]);
+                            i += 1;
+                            refresh_stickers(&mut stickers, puzzle);
+                        }
+                        event.inhibited = true;
+                    }
+                    _ => {}
                 }
             }
         }
